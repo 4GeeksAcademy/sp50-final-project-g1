565d4f25839950b0ff99b71f203a6a54e327286e
Hector Chocobar-Torrejon <45291267+hchocobar@users.noreply.github.com>
2024-01-14 13:58:43 +0000

    fix: react files according best practice

--- src/front/js/pages/Home.jsx
+++ src/front/js/pages/Home.jsx
@@ -3,6 +3,7 @@ import { Context } from "../store/appContext";
 import rigoImageUrl from "../../img/rigo-baby.jpg";
 import "../../styles/home.css";
 
+
 export const Home = () => {
 	const { store, actions } = useContext(Context);
 
@@ -10,7 +11,7 @@ export const Home = () => {
 		<div className="text-center mt-5">
 			<h1>Hello Rigo!!</h1>
 			<p>
-				<img src={rigoImageUrl} />
+				<img src={rigoImageUrl}/>
 			</p>
 			<div className="alert alert-info">
 				{store.message || "Loading message from the backend (make sure your python backend is running)..."}
