4c270ad4435909c07c40310f3d455ab312a64ed1
TheLore85 <garofalo.digital@gmail.com>
2024-01-26 11:37:43 +0100

    frontend: resolving conflict

--- src/front/js/pages/auth/SignupLocation.js
+++ src/front/js/pages/auth/SignupLocation.js
@@ -1,5 +1,5 @@
-import React, {useContext, useEffect, useState} from "react";
-import { useNavigate, Link} from 'react-router-dom'
+import React, { useContext, useEffect, useState } from "react";
+import { useNavigate, Link } from 'react-router-dom'
 import { Context } from "../../store/appContext";
 
 
@@ -7,7 +7,7 @@ export default function SignupLocation() {
 
   const navigate = useNavigate()
 
-  const {store, actions} = useContext(Context)
+  const { store, actions } = useContext(Context)
 
   const [studioName, setStudioName] = useState('')
   const [country, setCountry] = useState('')
@@ -35,7 +35,7 @@ export default function SignupLocation() {
     await actions.newLocation(location)
     await actions.updatePro(pro)
 
-    await actions.getLocationsByPro(pro.id) 
+    await actions.getLocationsByPro(pro.id)
     console.log(store.currentLocations)
 
     console.log('clicking next')
@@ -44,7 +44,7 @@ export default function SignupLocation() {
 
 
   useEffect(() => {
-    if(store.isLoggedIn){
+    if (store.isLoggedIn) {
       const fetchData = async () => {
         const response = await actions.authentication(store.token)
         const proId = await response.logged_in_as
@@ -54,27 +54,28 @@ export default function SignupLocation() {
       }
       fetchData()
     }
-    
+
   }, [store.isLoggedIn])
 
 
 
   return (
     <>
-    {!store.isLoggedIn ? navigate("/login") :
-      <section id="signupPersonalData" className="bg-light d-flex align-items-center" style={{ minHeight: '80vh'}}>
-        <div className="container py-5">
+      {!store.isLoggedIn ? navigate("/login") :
+        <section id="signupPersonalData" className="bg-light d-flex align-items-center" style={{ minHeight: '80vh' }}>
+          <div className="container py-5">
 
 
-          <div className="col-md-7 col-lg-8 m-auto bg-white p-5" style={{border:"solid #D1D1D1 6px", borderRadius:"18px"}}>
-            <h5>Studio Details</h5>
-            <hr />
-            <form className="needs-validation" noValidate="" onSubmit={(e)=>handleNext(e)}>
-              <h6 className="text-black-50">Location</h6>
-              <div className="p-4 mb-5 rounded" style={{backgroundColor:"#E0F3F3"}}>
+            <div className="col-md-7 col-lg-8 m-auto bg-white p-5" style={{ border: "solid #D1D1D1 6px", borderRadius: "18px" }}>
+              <h5>Studio Details</h5>
+              <hr />
+              <form className="needs-validation" noValidate="" onSubmit={(e) => handleNext(e)}>
+
+                <h6 className="text-black-50">Location</h6>
+                <div className="p-4 mb-5 rounded" style={{ backgroundColor: "#E0F3F3" }}>
                   <div className="mb-3">
                     <label htmlFor="firstName" className="form-label" >Studio Name</label>
-                    <input type="text" className="form-control" id="studioName" placeholder="Studio in Madrid" required="" value={studioName} onChange={(e) => setStudioName(e.target.value)}/>
+                    <input type="text" className="form-control" id="studioName" placeholder="Studio in Madrid" required="" value={studioName} onChange={(e) => setStudioName(e.target.value)} />
                     <div className="invalid-feedback">
                       Valid name is required.
                     </div>
@@ -83,7 +84,7 @@ export default function SignupLocation() {
 
                   <div className="mb-3">
                     <label htmlFor="country" className="form-label">Country</label>
-                    <input type="text" className="form-control" id="country" placeholder="Spain" required="" value={country} onChange={(e) => setCountry(e.target.value)}/>
+                    <input type="text" className="form-control" id="country" placeholder="Spain" required="" value={country} onChange={(e) => setCountry(e.target.value)} />
                     <div className="invalid-feedback">
                       Valid last name is required.
                     </div>
@@ -91,7 +92,7 @@ export default function SignupLocation() {
 
                   <div className="mb-3">
                     <label htmlFor="lastName" className="form-label">City</label>
-                    <input type="text" className="form-control" id="city" placeholder="Madrid" required="" value={city} onChange={(e) => setCity(e.target.value)}/>
+                    <input type="text" className="form-control" id="city" placeholder="Madrid" required="" value={city} onChange={(e) => setCity(e.target.value)} />
                     <div className="invalid-feedback">
                       Valid last name is required.
                     </div>
@@ -99,16 +100,15 @@ export default function SignupLocation() {
 
                   <div className="mb-3">
                     <label htmlFor="lastName" className="form-label">Address</label>
-                    <input type="text" className="form-control" id="address" placeholder="Calle Caballero 18" required="" value={address} onChange={(e) => setAddress(e.target.value)}/>
+                    <input type="text" className="form-control" id="address" placeholder="Calle Caballero 18" required="" value={address} onChange={(e) => setAddress(e.target.value)} />
                     <div className="invalid-feedback">
                       Valid last name is required.
                     </div>
                   </div>
+
                 </div>
 
-              </div>
-            
-            {/* 
+                {/* 
               <h6 className="text-black-50">Visit Duration</h6>
               <p className="small text-black-50 fw-light">Define how long it takes a visit slot in your studio</p>
               <div className="p-4 mb-5 rounded" style={{backgroundColor:"#E0F3F3"}}>
@@ -121,20 +121,20 @@ export default function SignupLocation() {
                 </div>
               </div> */}
 
-            
-            <div className="d-flex justify-content-between align-items-center border-top p-3">
-              <Link to="/signup/personal-data" className="text-decoration-none"><p className="text-black">{"<"} Back</p></Link>
-              <input type="submit" value="Next" className="btn btn-primary btn-lg" style={{backgroundColor:"#14C4B9", border:"none"}}></input>
+
+                <div className="d-flex justify-content-between align-items-center border-top p-3">
+                  <Link to="/signup/personal-data" className="text-decoration-none"><p className="text-black">{"<"} Back</p></Link>
+                  <input type="submit" value="Next" className="btn btn-primary btn-lg" style={{ backgroundColor: "#14C4B9", border: "none" }}></input>
+                </div>
+
+              </form>
+
             </div>
-            
-          </form>
 
           </div>
-
-        </div>
-      </section> 
+        </section>
       }
-    
+
     </>
   )
 }
