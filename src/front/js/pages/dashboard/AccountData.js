f4c2acd510c088120f012707a3cdcaed1d57e673
TheLore85 <garofalo.digital@gmail.com>
2024-02-08 14:50:31 +0100

    google api: login button

--- src/front/js/pages/dashboard/AccountData.js
+++ src/front/js/pages/dashboard/AccountData.js
@@ -1,8 +1,10 @@
 import React, { useContext, useEffect, useState } from "react";
 import { Context } from "../../store/appContext";
 import { useNavigate, Link } from "react-router-dom";
-import  GoogleLogin from  "@stack-pulse/next-google-login"
 
+// Google Login auth
+import { GoogleOAuthProvider } from '@react-oauth/google';
+import { GoogleLogin } from '@react-oauth/google';
 
 export default function AccountData() {
 
@@ -15,9 +17,12 @@ export default function AccountData() {
   const [userName, setUserName] = useState(store.currentPro.bookingpage_url || 'studiourl')
   const [phone, setPhone] = useState(store.currentPro.phone || '+34 615600600')
 
+  const googleId = process.env.GOOGLE_CLIENT_ID
+
 
   // Este efecto cargará SIEMPRE que se cargue el componente y isLoggedIn sea true. Trae todos los datos del pro a la store.
   useEffect(() => {
+    console.log('google id', googleId)
     const fetchData = async () => {
       try {
         const response = await actions.authentication(store.token);
@@ -54,50 +59,59 @@ export default function AccountData() {
 
   return (
     <>
-      {!store.isLoggedIn ? navigate('/login') :
-        <div className=" min-vh-100">
+      <GoogleOAuthProvider clientId={process.env.GOOGLE_CLIENT_ID}>
 
-          <div id='account-data' className="align-items-center bg-light py-5 container">
+        {!store.isLoggedIn ? navigate('/login') :
+          <div className=" min-vh-100">
 
-            <div className="text-black-50 mx-auto w-75" style={{ marginBottom: "6rem" }}>
-              <h4 className=" text-decoration-underline">ACCOUNT DATA</h4>
-              <div className="p-5 rounded bg-white border text-black-50">
-                <div><span>EMAIL: </span><span>{email}</span></div>
-                <div><span>USERNAME: </span><span>{userName}</span></div>
-                <div><span>PHONE: </span><span>{phone}</span></div>
-                <div><span>PASSWORX: </span><span>************</span></div>
+            <div id='account-data' className="align-items-center bg-light py-5 container">
+
+              <div className="text-black-50 mx-auto w-75" style={{ marginBottom: "6rem" }}>
+                <h4 className=" text-decoration-underline">ACCOUNT DATA</h4>
+                <div className="p-5 rounded bg-white border text-black-50">
+                  <div><span>EMAIL: </span><span>{email}</span></div>
+                  <div><span>USERNAME: </span><span>{userName}</span></div>
+                  <div><span>PHONE: </span><span>{phone}</span></div>
+                  <div><span>PASSWORX: </span><span>************</span></div>
+                </div>
               </div>
-            </div>
 
-            <div className=" text-black-50 mx-auto w-75" style={{ marginBottom: "6rem" }}>
-              <h4 className=" text-decoration-underline">MY BOOKING PAGE URL</h4>
-              <p>Share it with your patient or place it in your website to collect bookings</p>
-              <div className="p-3 rounded-3 border text-black-50" style={{ backgroundColor: "#E0F3F3" }}>
-                <div><Link to={`/${userName}`} >{`www.docdate.com/${userName}`}</Link></div>
+              <div className=" text-black-50 mx-auto w-75" style={{ marginBottom: "6rem" }}>
+                <h4 className=" text-decoration-underline">MY BOOKING PAGE URL</h4>
+                <p>Share it with your patient or place it in your website to collect bookings</p>
+                <div className="p-3 rounded-3 border text-black-50" style={{ backgroundColor: "#E0F3F3" }}>
+                  <div><Link to={`/${userName}`} >{`www.docdate.com/${userName}`}</Link></div>
+                </div>
               </div>
-            </div>
 
-            <div className="text-black-50 mx-auto w-75" style={{ marginBottom: "6rem" }}>
-              <h4 className=" text-decoration-underline">GOOGLE CALENDAR API CONNECTION</h4>
-              <p>Connect your google calendar with the DocDate agenda and keep all your events in one place</p>
-              <div className="p-5 rounded-3 bg-white border text-black-50">
-                <div><span><GoogleLogin 
-                          clientId={process.env.GOOGLE_CLIENT_ID}
-                          buttonText="Authorize Calendar"
-                          onSuccess={responseGoogle}
-                          onFailure={responseError}
-                          cookiePolicy={"single_host_origin"}
-                          responseType="code"
-                          accessType="offline"
-                          scope="openid email profile https://www.googleapis.com/auth/calendar" 
-                          /></span></div>
+              <div className="text-black-50 mx-auto w-75" style={{ marginBottom: "6rem" }}>
+                <h4 className=" text-decoration-underline">GOOGLE CALENDAR API CONNECTION</h4>
+                <p>Connect your google calendar with the DocDate agenda and keep all your events in one place</p>
+                <div className="p-5 rounded-3 bg-white border text-black-50">
+                  <div>
+
+                    {/*  GOOGLE LOGIN BUTTON */}
+                    <GoogleLogin
+                      onSuccess={credentialResponse => {
+                        console.log('GOOGLE LOGIN SUCCESS', credentialResponse);
+                      }}
+                      onError={() => {
+                        console.log('Login Failed');
+                      }}
+                    />
+
+
+
+                  </div>
+                </div>
               </div>
+
             </div>
 
           </div>
+        }
 
-        </div>
-      }
+      </GoogleOAuthProvider>
     </>
   )
 }
